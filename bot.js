723e1aed6fc9f65f39df1c88eda92eb86db7f9a0
Wido123 <55325182+Wido123@users.noreply.github.com>
2019-09-14 18:06:42 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+
+const Guild = "211543198651121664";
+const voiceChannel = "613749249968046123";
+
+client.on("ready", () => {
+  console.log(client.user.tag + " Ready! (" + client.guilds.get(Guild).name + ', ' + client.guilds.get(Guild).channels.get(voiceChannel).name + ')');
+  client.channels.get(voiceChannel).join().then(connection => {
+    console.log(client.user.tag + " Successfully connection.");
+    setInterval(() => {
+      if(!client.guilds.get(Guild).members.get(client.user.id).voiceChannel || client.guilds.get(Guild).members.get(client.user.id).voiceChannel && client.guilds.get(Guild).members.get(client.user.id).voiceChannelID != voiceChannel) {
+        client.channels.get(voiceChannel).join().then(connection => {
+          console.log(client.user.tag + " Successfully connection.");
+        });
+      }
+    }, 5000);
+  });
+});
+
+client.login(process.env.TOKEN);
